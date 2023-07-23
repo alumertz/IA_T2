78f879c3bfd663a114dcdddc2670f233b21e4faa
Igormartinssilva <92041109+Igormartinssilva@users.noreply.github.com>
2023-07-22 22:40:48 -0300

    questao 6 feita
    Spoiler da Barbie: ela morre no final e ninguem sabe Ken matou kkkkkkk

--- src/analysis.py
+++ src/analysis.py
@@ -64,9 +64,9 @@ def question3e():
     # If not possible, return 'NOT POSSIBLE'
 
 def question6():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    answerEpsilon = 12500
+    answerLearningRate = 0.9
+    return 'NOT POSSIBLE'
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
